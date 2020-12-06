214cf9f6fc39ce59b98e01e66671a54cabf67dc5
II-Zor-II <winzorJMpaelmo@yahoo.com.ph>
2020-12-06 14:47:45 +0800

    Summary: Added manual events input - section on the left side

--- resources/js/app.js
+++ resources/js/app.js
@@ -19,8 +19,9 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
+Vue.component('navbar-component', require('./components/NavBarComponent.vue').default);
+Vue.component('calendar-component', require('./components/CalendarComponent').default);
+Vue.component('manual-event-component', require('./components/ManualEventComponent').default);
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
