2587efb6f54d4f7f5f4a5effe55ffe1f3649a5e4
II-Zor-II <winzorJMpaelmo@yahoo.com.ph>
2021-01-02 16:02:54 +0800

    Summary: added TLDR personal description of JOIN queries for review

--- database/DB_review/Query_1_description.sql
+++ database/DB_review/Query_1_description.sql
@@ -1,27 +1,30 @@
-SELECT * FROM t_employee;
-
-SELECT * FROM t_project;
-
+-- use this to filter rows that exactly fits the condition
 -- Inner JOIN sample
 SELECT t_employee.employee_id, t_employee.first_name, t_project.project_id, t_project.project_name
 FROM t_employee
 INNER JOIN t_project
 ON t_employee.employee_id=t_project.employee_id;
 
+-- use this to retrieve rows that fits the condition + all rows from the "left table" in this case "t_employee"
 -- Left JOIN sample
 SELECT t_employee.employee_id, t_employee.first_name, t_employee.address, t_employee.contact_number, t_project.project_name
 FROM t_employee
 LEFT JOIN t_project
 ON t_employee.employee_id=t_project.employee_id;
 
+-- use this to retrieve rows that fits the condition + all rows from the "right table" in this case "t_project"
 -- Right JOIN sample
 SELECT t_project.project_id, t_project.project_name, t_employee.employee_id, t_employee.first_name, t_employee.address, t_employee.contact_number
 FROM t_employee
 RIGHT JOIN t_project
 ON t_employee.employee_id=t_project.employee_id;
 
--- Full JOIN sample
-SELECT t_project.project_id, t_project.project_name, t_employee.employee_id, t_employee.first_name, t_employee.address, t_employee.contact_number
+-- use this to retrieve all rows from both table
+-- Full JOIN mysql emulation
+SELECT t_project.project_id, t_project.project_name, t_employee.employee_id, t_employee.email
 FROM t_employee
-RIGHT JOIN t_project
-ON t_employee.employee_id=t_project.employee_id;
+LEFT JOIN t_project ON t_employee.employee_id=t_project.employee_id
+UNION
+SELECT t_project.project_id, t_project.project_name, t_employee.employee_id, t_employee.email
+FROM t_employee
+RIGHT JOIN t_project ON t_employee.employee_id=t_project.employee_id;
